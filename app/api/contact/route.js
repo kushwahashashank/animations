df0f360fb432512f5bfec5b495ac5d68163d00c0
Abhishek Kushwaha <abhishekkushwaha@Abhisheks-MacBook-Air.local>
2024-11-30 21:50:40 +0530

    adding mail preview 1

--- app/api/contact/route.js
+++ app/api/contact/route.js
@@ -64,10 +64,10 @@ export async function POST(req) {
     });
 
     const mailOptions = {
-      from: EMAIL_USER,
+      from: `You Know Me <${EMAIL_USER}>`,
       to: email,
       subject: "Thank you for contacting us!",
-      text: `You Know Me\n\nHello ${name},\n\nThank you for reaching out. We have received your message: "${message}".\n\nWe will get back to you shortly!\n\nBest regards,\nAbhishek Kushwaha `,
+      text: `Hello ${name},\n\nThank you for reaching out. We have received your message: "${message}".\n\nWe will get back to you shortly!\n\nBest regards,\nAbhishek Kushwaha `,
     };
 
     await transporter.sendMail(mailOptions);
